fa3af355f731dd2b62abe18768e080a504abf936
Thomas Kortyka <info@toxictoast.de>
2021-06-10 09:10:08 +0200

    add dockerfile, add workflows, add post routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Logger, Post } from "@nestjs/common";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('join')
+  userJoined(@Body() body: { channel: string, username: string }): void {
+    Logger.debug(body, 'Join');
   }
+
+  @Post('part')
+  userPart(@Body() body: { channel: string, username: string }): void {
+    Logger.debug(body, 'Part');
+  }
+
 }
